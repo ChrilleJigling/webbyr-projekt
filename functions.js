d37a30696b26352fb4cabf8bebc2808e285bdb62
ChrilleJigling <christianjigling@gmail.com>
2024-09-24 15:01:46 +0200

    Big update

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
+/* Nav - hide when scroll down and reappear when scroll up */
 let lastScrollTop = 0;
-
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     var navbarChecklist;
@@ -25,6 +25,8 @@ window.addEventListener('scroll', function() {
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
 });
 
+
+/* FAQ section - click & collapse the FAQ's */
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
@@ -40,22 +42,26 @@ faqItems.forEach(item => {
     });
 });
 
+/* Mobile Hamburger Menu */
 const menuIcon = document.getElementById('menu-icon');
 const navMenu = document.getElementById('nav-menu');
-
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('open');
     navMenu.classList.toggle('open');
 });
 
+
+/* Scroll To Element function with id as parameter */
 function scrollToElement(id) {
     const element = document.getElementById(id);  // Get the element by its ID
+    console.log(element);
     if (element) {
         element.scrollIntoView({
             behavior: 'smooth', // Smooth scrolling
-            block: 'center'     // Center the element vertically in the viewport
+            block: 'start'     // Center the element vertically in the viewport
         });
     } else {
         console.warn(`Element with ID '${id}' not found.`);
     }
 }
+
