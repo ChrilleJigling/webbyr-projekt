160eb85e5b8fbace3b3772ed9d28c874cebe9036
ChrilleJigling <christianjigling@gmail.com>
2024-10-04 10:04:40 +0200

    Update

--- functions.js
+++ functions.js
@@ -1,3 +1,71 @@
+$(document).ready(function() {
+    if ($(window).width() > 1025) {
+        var e = 0;
+        window.addEventListener("mouseout", function(o) {
+            e = o.clientY;
+            if (e < 0) {
+                $("#exit-booking-popup").addClass("show-popup");
+            }
+        }, false);
+        // Add this only once
+        $("#exit-booking-popup").on("mouseleave", function() {
+            $("#exit-booking-popup").removeClass("show-popup");
+        });
+    }
+});
+
+
+/* $( document ).ready(function() {
+    $(window).width() > 1025 && ($(function() {
+        var e = 0;
+        window.addEventListener("mouseout", function(o) {
+            (e = o.clientY) < 0 ? $("#exit-booking-popup").addClass("show-popup") : $("#exit-booking-popup").on("mouseleave", function(e) {
+                $("#exit-booking-popup").removeClass("show-popup")
+            })
+        }, !1)
+    })
+});
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    !function e(o, t) {
+        var s = document.querySelectorAll(".hours")
+          , n = document.querySelectorAll(".minutes")
+          , r = document.querySelectorAll(".seconds");
+        function i() {
+            var o, i, a = {
+                total: i = Date.parse(o = t) - Date.parse(new Date),
+                hours: Math.floor(i / 36e5 % 24),
+                minutes: Math.floor(i / 1e3 / 60 % 60),
+                seconds: Math.floor(i / 1e3 % 60)
+            };
+            if (a.total <= 0) {
+                clearInterval(l);
+                for (var c = Date.parse(t), u = Date.parse(new Date); c <= u; )
+                    c += 864e5;
+                e("countdown", new Date(c))
+            } else
+                s.forEach(e => {
+                    e.innerHTML = ("0" + a.hours).slice(-2),
+                    0 == a.minutes ? e.classList.add("bounce-number") : e.classList.remove("bounce-number")
+                }
+                ),
+                n.forEach(e => {
+                    e.innerHTML = ("0" + a.minutes).slice(-2),
+                    0 == a.seconds ? e.classList.add("bounce-number") : e.classList.remove("bounce-number")
+                }
+                ),
+                r.forEach(e => {
+                    e.innerHTML = ("0" + a.seconds).slice(-2),
+                    e.classList.add("bounce-number")
+                }
+                )
+        }
+        i();
+        var l = setInterval(i, 1e3)
+    }("countdown", "December 7 2019 00:00:00 GMT+0200")
+}, !1);
+
 /* Nav - hide when scroll down and reappear when scroll up */
 let lastScrollTop = 0;
 window.addEventListener('scroll', function() {
@@ -42,6 +110,20 @@ faqItems.forEach(item => {
     });
 });
 
+ // Check for URL parameter and scroll to the specific section
+ window.onload = function() {
+    // Get the URL query parameters
+    var params = new URLSearchParams(window.location.search);
+    
+    // Check if the 'scrollTo' parameter exists
+    if (params.has('scrollTo')) {
+        var sectionId = params.get('scrollTo');
+        
+        // Call the scrollToElement function with the value from the parameter
+        scrollToElement(sectionId);
+    }
+};
+
 /* Mobile Hamburger Menu */
 const menuIcon = document.getElementById('menu-icon');
 const navMenu = document.getElementById('nav-menu');
@@ -63,5 +145,10 @@ function scrollToElement(id) {
     } else {
         console.warn(`Element with ID '${id}' not found.`);
     }
+}
+
+function redirectToIndex(element) {
+    // Redirect to index.html with a query parameter
+    window.location.href = "index.html#"+element;
 }
 
